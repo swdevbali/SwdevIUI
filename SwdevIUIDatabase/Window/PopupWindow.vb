811904fd16f272d1552122cf866419384e6b351e
unknown <Eko SW@Windows7Macbook.(none)>
2011-12-08 11:29:26 +0700

    database popup window, will me modify into embedded

---
+++ SwdevIUIDatabase/Window/PopupWindow.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Windows.Forms
+Imports SwdevIUI
+
+Public Class PopupWindow
+
+    Private Sub PopupWindow_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        e.Cancel = True
+        Hide()
+    End Sub
+
+    Sub clearControl()
+        pnlUtama.Controls.Clear()
+    End Sub
+
+    Sub Add(ByVal FormEntry As PageEntryTemplate)
+        pnlUtama.Controls.Add(FormEntry)
+    End Sub
+
+End Class
