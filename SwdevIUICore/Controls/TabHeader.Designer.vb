3325e5ae7f6f50b5d92594585ce630903c8ae0da
unknown <Eko SW@Windows7Macbook.(none)>
2011-12-30 09:23:04 +0700

    Adding TabHeader control for subtitution of BreadCrumb, to allow tabbed document

---
+++ SwdevIUICore/Controls/TabHeader.Designer.vb
@@ -0,0 +1,54 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TabHeader
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.tablePage = New System.Windows.Forms.TableLayoutPanel()
+        Me.SuspendLayout()
+        '
+        'tablePage
+        '
+        Me.tablePage.ColumnCount = 2
+        Me.tablePage.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120.0!))
+        Me.tablePage.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.tablePage.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tablePage.Location = New System.Drawing.Point(0, 0)
+        Me.tablePage.Name = "tablePage"
+        Me.tablePage.RowCount = 1
+        Me.tablePage.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.tablePage.Size = New System.Drawing.Size(808, 31)
+        Me.tablePage.TabIndex = 0
+        '
+        'TabHeader
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Transparent
+        Me.Controls.Add(Me.tablePage)
+        Me.Name = "TabHeader"
+        Me.Size = New System.Drawing.Size(808, 31)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents tablePage As System.Windows.Forms.TableLayoutPanel
+
+End Class
---
+++ SwdevIUICore/Controls/TabHeader.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Windows.Forms
+
+Public Class TabHeader
+    Dim hashTab As New Hashtable
+
+    Event EnterPage(ByVal page As PageTemplate, ByVal eventArgs As EventArgs)
+
+    Sub add(ByVal view As PageTemplate)
+        If hashTab(view.Title) IsNot Nothing Then
+            Return
+        End If
+        hashTab.Add(view.Title, view)
+        Dim linkTab As New LinkLabel
+
+        linkTab.Text = view.Title
+        linkTab.Dock = DockStyle.Fill
+        linkTab.TextAlign = Drawing.ContentAlignment.MiddleCenter
+        linkTab.BackColor = Drawing.Color.Transparent
+        AddHandler linkTab.MouseEnter, AddressOf linkHoverIn
+        AddHandler linkTab.MouseLeave, AddressOf linkHoverOut
+        AddHandler linkTab.Click, AddressOf linkClicked
+
+        If tablePage.ColumnCount = 2 Then 'first we have one column. it's the minimum
+            tablePage.Controls.Add(linkTab, tablePage.ColumnCount - 2, 0)
+            tablePage.ColumnStyles(tablePage.ColumnCount - 2).SizeType = SizeType.Absolute
+            tablePage.ColumnStyles(tablePage.ColumnCount - 2).Width = 100
+            tablePage.ColumnCount = tablePage.ColumnCount + 1
+           
+        Else
+         
+            tablePage.Controls.Add(linkTab, tablePage.ColumnCount - 2, 0)
+            tablePage.ColumnCount = tablePage.ColumnCount + 2
+            ' tablePage.ColumnStyles(tablePage.ColumnCount - 2).SizeType = SizeType.Absolute
+            'tablePage.ColumnStyles(tablePage.ColumnCount - 2).Width = 120
+        End If
+
+    End Sub
+    Sub linkHoverIn(ByVal sender As Object, ByVal e As EventArgs)
+        Dim linkTab As LinkLabel = sender
+        linkTab.BackColor = Drawing.Color.White
+    End Sub
+    Sub linkHoverOut(ByVal sender As Object, ByVal e As EventArgs)
+        Dim linkTab As LinkLabel = sender
+        linkTab.BackColor = Drawing.Color.Transparent
+    End Sub
+    Sub clearNode()
+        tablePage.Controls.Clear()
+        tablePage.ColumnCount = 2
+        tablePage.ColumnStyles(tablePage.ColumnCount - 2).SizeType = SizeType.Absolute
+        tablePage.ColumnStyles(tablePage.ColumnCount - 2).Width = 120
+    End Sub
+
+    Sub goBack()
+        'Throw New NotImplementedException
+    End Sub
+    Sub linkClicked(ByVal sender As System.Object, ByVal e As EventArgs)
+
+        Dim link As LinkLabel = sender
+        Dim page As PageTemplate
+        page = hashTab(link.Text)
+
+        'learn from http://stackoverflow.com/questions/2594608/asp-net-vb-user-control-raising-an-event-on-the-calling-page
+        RaiseEvent EnterPage(page, New EventArgs())
+    End Sub
+End Class
